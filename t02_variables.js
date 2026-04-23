4e0cb03f7c9d05f9f88941a1925cf16938eab3ef
jess-low <24135jl@hvhs.school.nz>
2026-04-23 02:23:52 +0000

    Apr 23, 2026, 2:23 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,10 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js");
+console.log("HELLO!");
+
+// This is a single line comment
+
+var 
