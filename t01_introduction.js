ce99abec51577d589cc8a22140eb41d3a8730939
jess-low <24135jl@hvhs.school.nz>
2026-04-23 02:19:30 +0000

    Apr 23, 2026, 2:19 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -2,8 +2,8 @@
 This is a block comment.
 Put your header comment here!
 ****************************/
-console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Running t01_introduction.js");
+console.log("HELLO!");
 
 // This is a single line comment
 
