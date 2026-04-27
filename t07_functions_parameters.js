0a54713a68a37758b061b672ae4ec3d31b49627f
jess-low <24135jl@hvhs.school.nz>
2026-04-27 22:36:46 +0000

    Apr 27, 2026, 10:36 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -8,28 +8,17 @@ console.log("Running t07_functions_parameters.js");
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
 
-// This code defines the variables and their properties
-
-let aName = "Bob";
-let year = 2026;
-let age = 38;
-let birth = year - age;
-let newAge = age + 10;
-let money = 10;
-
 //Functions
 function welcome(){
     OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
+}
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<h2>" + _name + ": $" + _price + "</h2>";
 }
 
 // This code adds a message to console log
 
 welcome()
-OUTPUT.innerHTML += "<h3>Hi " + aName + ".</h3>";
-OUTPUT.innerHTML += "<h4>As of " + year + ", you are " + age + " years old.</h4>";
-OUTPUT.innerHTML += "<p>You were born in " + birth + ".</p>";
-OUTPUT.innerHTML += "<p>In ten years you will be " + newAge + " years old.</p>";
-OUTPUT.innerHTML += "<p>You have " + money + " dollars.</p>";
-welcome()
-OUTPUT.innerHTML += "<p>You spend half of your money, now you have $" + (money = money / 2) + ".</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have $" + (money + 3) + ".</p>";
+displayProduct("Chocolate bar", 4);
+displayProduct("Chips", 3);
+displayProduct("Drink", "2.50");
