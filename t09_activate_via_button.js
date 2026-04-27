f3e5d87a4dcbd27e8198531ab60d268ae133bf4c
jess-low <24135jl@hvhs.school.nz>
2026-04-27 22:46:52 +0000

    Apr 27, 2026, 10:46 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,19 @@
-/* Paste your code from task 7 here*/
+/****************************
+Intro:
+****************************/
+
+console.log("Running t09_activate_via_button.js");
+
+// transfer from JS to HTML
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
+
+//Functions
+function welcome(){
+    OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
+}
+
+// This code adds a message to console log
+
+welcome()
+welcome()
