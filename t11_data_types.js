4e27e9252ee740dc215fd4efba253c1adb81aacf
jess-low <24135jl@hvhs.school.nz>
2026-04-30 02:33:43 +0000

    Apr 30, 2026, 2:33 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -10,16 +10,13 @@ OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
 
 
 //Functions
-function welcome(){
-    OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
-}
+
 function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     let userName = NAME_FIELD.value;
     OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>"
   }
 function start(){
-    form()
-    welcome()
+    getFormInput()
 }
 
