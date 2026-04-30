509a6dc32f9d5403a5e19c987d36bfb9acb4e7a4
jess-low <24135jl@hvhs.school.nz>
2026-04-30 02:27:12 +0000

    Apr 30, 2026, 2:27 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -7,8 +7,6 @@ console.log("Running t10_input_from_HTML.js");
 // transfer from JS to HTML
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
-const NAME_FIELD = document.getElementById("nameField");
-let userName = NAME_FIELD.value;
 
 
 //Functions
@@ -16,7 +14,9 @@ function welcome(){
     OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
 }
 function getFormInput(){
-  
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>"
   }
 function start(){
     form()
