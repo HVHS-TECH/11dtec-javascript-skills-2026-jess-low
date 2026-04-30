0784835d8306914d8dbf660c175b0487ab923390
jess-low <24135jl@hvhs.school.nz>
2026-04-30 02:59:12 +0000

    Apr 30, 2026, 2:59 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,33 @@
-/* Paste your code from the last task */
+/****************************
+Intro:
+****************************/
+
+console.log("Running t11_data_types.js");
+
+// transfer from JS to HTML
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
+
+
+//Functions
+function getUserName(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>"
+  }
+function getUserAge(){
+    const AGE_FIELD = document.getElementById("ageField");
+    let userAge = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>"
+  }
+  function getUserMoney(){
+    const MONEY_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(MONEY_FIELD.value);
+    OUTPUT.innerHTML += "<p>You have $" + userMoney + ".</p>"
+  }
+function start(){
+    getUserName();
+    getUserAge();
+    getUserMoney();
+}
+
