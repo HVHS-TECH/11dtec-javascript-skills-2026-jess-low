47d761e48ba4a288cf15af52deb75e9b9a7e7082
jess-low <24135jl@hvhs.school.nz>
2026-05-07 03:13:08 +0000

    May 7, 2026, 3:13 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,52 @@
-/* Paste your code from the last task */
+/****************************
+Intro:/
+****************************/
+
+console.log("Running t16_arrays_continued.js");
+
+// transfer from JS to HTML
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
+
+
+//Functions
+  function getBottles(){
+    const BOTTLE_FIELD = document.getElementById("bottleField");
+    let bottleAmount = Number(BOTTLE_FIELD.value).toFixed(0);
+    for (let bottle = bottleAmount; bottle>-1; bottle--) {
+      if (bottle > 2){
+        OUTPUT.innerHTML += "<p>" + bottle + " bottles of milk on the wall</p><p>" + bottle + " bottles of milk</p><p>If one of those bottles should happen to fall</p><p>" + (bottle - 1) + " bottles of milk on the wall.</p><br>";
+      } else if (bottle == 2){
+        OUTPUT.innerHTML += "<p>" + bottle + " bottles of milk on the wall</p><p>" + bottle + " bottles of milk</p><p>If one of those bottles should happen to fall</p><p>" + (bottle - 1) + " bottle of milk on the wall.</p><br>";
+      } else if (bottle == 1){
+        OUTPUT.innerHTML += "<p>" + bottle + " bottle of milk on the wall</p><p>" + bottle + " bottle of milk</p><p>If that last bottle should happen to fall</p><p>No more bottles of milk on the wall.</p><br>";
+      } else {
+        OUTPUT.innerHTML += "<p>No bottles of milk on the wall</p><p>No bottles of milk</p><p>If there are no bottles left to fall</p><p>Were there ever bottles of milk on the wall?</p>"
+      }
+    }
+  }
+
+  
+let chocolate = ["You loathe chocolate.", "Chocolate is meh.", "Chocolate is pretty good.", "Chocolate is the best thing EVER!!!"];
+
+  function getChocLike(){
+    const CHOC_FIELD = document.getElementById("chocField");
+    let chocLike = Number(CHOC_FIELD.value).toFixed(0);
+    if (chocLike < 0){
+        OUTPUT.innerHTML += "<p>Your disgust for chocolate is off the scale!</p>"
+    } else if (chocLike <= 3){
+    OUTPUT.innerHTML += "<p>" + chocolate[chocLike] + "</p>";
+} else {
+    OUTPUT.innerHTML += "<p>Your love for chocolate is off the scale!</p>"
+}
+  }
+  
+
+  
+
+// This code adds a message to the page
+
+function start(){
+    getBottles();
+    getChocLike();
+    }
