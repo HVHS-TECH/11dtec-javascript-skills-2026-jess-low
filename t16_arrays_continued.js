6fabe99adf54452a16af7ff3915456c75872075c
jess-low <24135jl@hvhs.school.nz>
2026-05-10 21:30:20 +0000

    May 10, 2026, 9:30 PM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -10,49 +10,21 @@ OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
 
 
 //Functions
-  function getBottles(){
-    const BOTTLE_FIELD = document.getElementById("bottleField");
-    let bottleAmount = Number(BOTTLE_FIELD.value).toFixed(0);
-    for (let bottle = bottleAmount; bottle>-1; bottle--) {
-      if (bottle > 2){
-        OUTPUT.innerHTML += "<p>" + bottle + " bottles of milk on the wall</p><p>" + bottle + " bottles of milk</p><p>If one of those bottles should happen to fall</p><p>" + (bottle - 1) + " bottles of milk on the wall.</p><br>";
-      } else if (bottle == 2){
-        OUTPUT.innerHTML += "<p>" + bottle + " bottles of milk on the wall</p><p>" + bottle + " bottles of milk</p><p>If one of those bottles should happen to fall</p><p>" + (bottle - 1) + " bottle of milk on the wall.</p><br>";
-      } else if (bottle == 1){
-        OUTPUT.innerHTML += "<p>" + bottle + " bottle of milk on the wall</p><p>" + bottle + " bottle of milk</p><p>If that last bottle should happen to fall</p><p>No more bottles of milk on the wall.</p><br>";
-      } else {
-        OUTPUT.innerHTML += "<p>No bottles of milk on the wall</p><p>No bottles of milk</p><p>If there are no bottles left to fall</p><p>Were there ever bottles of milk on the wall?</p>"
-      }
-    }
-  }
-
   
-let chocolate = ["You loathe chocolate.", "Chocolate is meh.", "Chocolate is pretty good.", "Chocolate is the best thing EVER!!!"];
-
-  function getList(){
-    const LIST_FIELD = document.getElementById("listField");
-    let list = Number(LIST_FIELD.value).toFixed(0);
-    if (list < 0){
-        OUTPUT.innerHTML += "<p>Your disgust for chocolate is off the scale!</p>"
-    } else if (list <= 3){
-    OUTPUT.innerHTML += "<p>" + chocolate[list] + "</p>";
-} else {
-    OUTPUT.innerHTML += "<p>Your love for chocolate is off the scale!</p>"
-}
-  }
+let listArray = [];
 
   function getItem(){
     const ITEM_FIELD = document.getElementById("itemField");
     let item = ITEM_FIELD.value;
+    listArray.push(item);
     OUTPUT.innerHTML += "<p>You have added '" + item + "' to the list.</p>"
   }
-  
-
-  
 
 // This code adds a message to the page
 
-function start(){
-    getBottles();
-    getChocLike();
+function getList(){
+  OUTPUT.innerHTML += "<p>These are the items on your shopping list:"
+  for (let i=0; i<listArray.length; i++){
+    OUTPUT.innerHTML += "<p>Item " + (i+1) + ": " + listArray[i] + "</p>";
+  }
     }
