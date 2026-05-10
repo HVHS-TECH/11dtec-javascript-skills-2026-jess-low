5b8c96c011a2664c4df3d8061d4b2dca41e3a11a
jess-low <24135jl@hvhs.school.nz>
2026-05-10 21:22:03 +0000

    May 10, 2026, 9:22 PM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -43,8 +43,8 @@ let chocolate = ["You loathe chocolate.", "Chocolate is meh.", "Chocolate is pre
 
   function getItem(){
     const ITEM_FIELD = document.getElementById("itemField");
-    let item = ITEM_FIELD;
-    OUTPUT.innerHTML += "<p>You have added " + item + " to the list.</p>"
+    let item = ITEM_FIELD.value;
+    OUTPUT.innerHTML += "<p>You have added '" + item + "' to the list.</p>"
   }
   
 
