60f51fe972786190605689f46ee2e4bce2cc7e4a
jess-low <24135jl@hvhs.school.nz>
2026-05-11 22:25:35 +0000

    May 11, 2026, 10:25 PM

--- t18_validation.js
+++ t18_validation.js
@@ -1,1 +1,82 @@
-/* Paste your code from the last task */
+/****************************
+Intro:/
+****************************/
+
+console.log("Running t18_validation.js");
+
+// transfer from JS to HTML
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
+
+
+//Functions
+
+function welcome(){
+    OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
+}
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<h2>" + _name + ": $" + _price + "</h2>";
+}
+function getUserName(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>";
+  }
+function getUserAge(){
+    const AGE_FIELD = document.getElementById("ageField");
+    let userAge = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>";
+  }
+  function getUserMoney(){
+    const MONEY_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(MONEY_FIELD.value).toFixed(2);
+    OUTPUT.innerHTML += "<p>You have $" + userMoney + ".</p>";
+return userMoney;
+  }
+function calculateChange(_name, _cost){
+    const MONEY_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(MONEY_FIELD.value).toFixed(2);
+    OUTPUT.innerHTML += "<p> You have $" + userMoney + ". If you buy a " + _name + ", you will get $" + (userMoney - _cost).toFixed(2) + " change.</p>"
+}
+  function afford(){
+    const MONEY_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(MONEY_FIELD.value);
+if (userMoney >= 4){
+    OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can afford a chocolate bar.</p>";
+    calculateChange("chocolate bar", 4);
+    calculateChange("pack of chips", 3);
+    calculateChange("drink", 2.50);
+} else {
+    OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can't afford a chocolate bar.</p>";
+}
+return userMoney;
+  }
+  
+let listArray = [];
+
+  function getItem(){
+    const ITEM_FIELD = document.getElementById("itemField");
+    let item = ITEM_FIELD.value;
+    listArray.push(item);
+    OUTPUT.innerHTML += "<p>You have added '" + item + "' to the list.</p>"
+  }
+
+// This code adds a message to the page
+
+function getList(){
+  OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>"
+  for (let i=0; i<listArray.length; i++){
+    OUTPUT.innerHTML += "<p>Item " + (i+1) + ": " + listArray[i] + "</p>";
+  }
+    }
+
+function start(){
+  welcome();
+  displayProduct("Chocolate Bar", 4);
+  displayProduct("Chips", 3);
+  displayProduct("Drink", "2.50");
+    getUserName();
+    getUserAge();
+    getUserMoney();
+    afford();
+}
