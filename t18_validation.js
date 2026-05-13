d7732e5480c58a0f1108b132973657b68c07262d
jess-low <24135jl@hvhs.school.nz>
2026-05-13 01:19:59 +0000

    May 13, 2026, 1:19 AM

--- t18_validation.js
+++ t18_validation.js
@@ -21,7 +21,7 @@ function getUserName(){
     const NAME_FIELD = document.getElementById("nameField");
     if (NAME_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
-    } else if (NAME_FIELD = Number(NAME_FIELD.value)){
+    } else if (NAME_FIELD == Number(NAME_FIELD.value)){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
     } else{
     let userName = NAME_FIELD.value;
