1b71a95a41bb9fb490b409b8ca31c218e2afe7b7
jess-low <24135jl@hvhs.school.nz>
2026-05-14 02:58:33 +0000

    May 14, 2026, 2:58 AM

--- t21_objects.js
+++ t21_objects.js
@@ -17,26 +17,33 @@ function welcome(){
 function displayProduct(_name, _price){
     OUTPUT.innerHTML += "<h2>" + _name + ": $" + _price + "</h2>";
 }
+
+var user = {
+    name: "undefined",
+    age: 0,
+    money: 0,
+}
+
 function getUserName(){
     const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
+    user.name = NAME_FIELD.value;
     if (NAME_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
-    } else if (userName.length < 2){
+    } else if (user.name.length < 2){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
     } else {
-    OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>";
+    OUTPUT.innerHTML += "<p>Your name is " + user.name + ".</p>";
 }
   }
 function getUserAge(){
     const AGE_FIELD = document.getElementById("ageField");
-    let userAge = Number(AGE_FIELD.value);
+    user.age = Number(AGE_FIELD.value);
     if (AGE_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
-    } else if (userAge > 130){
-        OUTPUT.innerHTML += "<p>You're " + userAge + "? Wow, that's old! Are you sure you're not immortal?</p>"
+    } else if (user.age > 130){
+        OUTPUT.innerHTML += "<p>You're " + user.age + "? Wow, that's old! Are you sure you're not immortal?</p>"
     } else {
-    OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>";
+    OUTPUT.innerHTML += "<p>You are " + user.age + " years old.</p>";
 }
   }
   function getUserMoney(){
@@ -44,9 +51,8 @@ function getUserAge(){
     if (MONEY_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
     } else {
-    let userMoney = Number(MONEY_FIELD.value).toFixed(2);
-    OUTPUT.innerHTML += "<p>You have $" + userMoney + ".</p>";
-return userMoney;
+    user.money = Number(MONEY_FIELD.value).toFixed(2);
+    OUTPUT.innerHTML += "<p>You have $" + user.money + ".</p>";
     }
   }
 function calculateChange(_name, _cost){
@@ -54,36 +60,33 @@ function calculateChange(_name, _cost){
     if (MONEY_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
     } else {
-    let userMoney = Number(MONEY_FIELD.value).toFixed(2);
-    OUTPUT.innerHTML += "<p> You have $" + userMoney + ". If you buy a " + _name + ", you will get $" + (userMoney - _cost).toFixed(2) + " change.</p>"
+    user.money = Number(MONEY_FIELD.value).toFixed(2);
+    OUTPUT.innerHTML += "<p> You have $" + userMoney + ". If you buy a " + _name + ", you will get $" + (user.money - _cost).toFixed(2) + " change.</p>"
 }
 }
+
   function afford(){
     const MONEY_FIELD = document.getElementById("moneyField");
     if (MONEY_FIELD.checkValidity() === false){
         OUTPUT.innerHTML = "<h1>Please fill out all fields correctly.</h1>"
-    } else {
-    let userMoney = Number(MONEY_FIELD.value);
-if (userMoney >= 4){
+    } else if (user.money >= 4){
     OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can afford a chocolate bar.</p>";
     calculateChange("chocolate bar", 4);
     calculateChange("pack of chips", 3);
     calculateChange("drink", 2.50);
-} else if (userMoney >= 3){
+} else if (user.money >= 3){
     OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can't afford a chocolate bar.</p>";
     calculateChange("pack of chips", 3);
     calculateChange("drink", 2.50);
-} else if (userMoney >= 2.50){
+} else if (user.money >= 2.50){
     OUTPUT.innerHTML += "<p>A chocolate bar costs $4 and a pack of chips costs $3, so you can't afford a chocolate bar or a pack of chips.</p>";
     calculateChange("drink", 2.50);
-} else if (userMoney >= 0){
+} else if (user.money >= 0){
     OUTPUT.innerHTML += "<p>A chocolate bar costs $4, pack of chips costs $3, and a drink costs $2.50, so you can't afford any of them. Come back when you're not broke.</p>";
 } else {
     OUTPUT.innerHTML += "<p>Hold on. Negative numbers? You're not just broke, you're in DEBT. Go get a job!</p>"
 }
-return userMoney;
   }
-}
   
 let listArray = [];
 
